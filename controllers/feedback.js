e99f634586ee5003303fcd58125f29e298ec4bb2
AkanWealth <akan.asanga@gmail.com>
2020-12-15 08:02:15 +0100

    nodemailer done for both contact and feedback page

--- controllers/feedback.js
+++ controllers/feedback.js
@@ -1,4 +1,4 @@
-// const nodemailer = require('nodemailer')
+const nodemailer = require('nodemailer')
 
 const connection = require('../modules/db')
 module.exports = (app) => {
@@ -14,6 +14,32 @@ module.exports = (app) => {
             } else {
                 return err;
             }
+            var transporter = nodemailer.createTransport({
+                host: "smtp.mailtrap.io",
+                port: 2525,
+                auth: {
+                    user: "9f06e003b14b45",
+                    pass: "e32d15f2d5cfff"
+                }
+            });
+
+            const mailOptions = {
+                from: 'akan.asanga@gmail.com',
+                to: 'akan.asanga@yahoo.com',
+                subject: 'From Feedback',
+                html: `<h1>Contact-us</h1> \n
+            '${req.body.fullName}', \n
+            '${req.body.email}',\n
+            '${req.body.message}'`
+            };
+
+            transporter.sendMail(mailOptions, function(error, info) {
+                if (error) {
+                    console.log(error);
+                } else {
+                    console.log('Email sent: ' + info.response);
+                }
+            });
         })
     })
 
@@ -27,13 +53,48 @@ module.exports = (app) => {
         '${req.body.message}',
         '${req.body.date}')`
         connection.query(mysql, (err, respond) => {
-            if (respond) {
-                res.send("Thank you! We will get back to you")
+                if (respond) {
+                    res.send("Thank you! We will get back to you")
 
-            } else {
-                return err;
+                } else {
+                    return err;
+                }
+            })
+            // const transporter = nodemailer.createTransport({
+            //     service: 'gmail',
+            //     auth: {
+            //         user: '@gmail.com',
+            //         pass: 'password' // naturally, replace both with your real credentials or an application-specific password
+            //     }
+            // });
+
+        var transporter = nodemailer.createTransport({
+            host: "smtp.mailtrap.io",
+            port: 2525,
+            auth: {
+                user: "9f06e003b14b45",
+                pass: "e32d15f2d5cfff"
             }
-        })
+        });
+
+        const mailOptions = {
+            from: 'akan.asanga@gmail.com',
+            to: 'akan.asanga@yahoo.com',
+            subject: 'From contact',
+            html: `<h1>Contact-us</h1> \n
+            '${req.body.contactName}', \n
+            '${req.body.contactEmail}',\n
+            '${req.body.message}',\n
+            '${req.body.date}'`
+        };
+
+        transporter.sendMail(mailOptions, function(error, info) {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log('Email sent: ' + info.response);
+            }
+        });
     })
 
 }
