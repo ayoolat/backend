983168cd105840d354c06722f12c286895b26051
ayoola_toluwanimi@yahoo.com <ayoola_toluwanimi@yahoo.com>
2020-12-31 15:08:19 +0100

    .

--- modules/schema.js
+++ modules/schema.js
@@ -4,9 +4,9 @@ const schemas = {
   signUp: Joi.object().keys({ 
     companyName: Joi.string().alphanum().min(3).max(15).required, 
     companyType: Joi.string().required(),
-    password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
-    repeat_password: Joi.ref('password').required(),
-    email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).required()
+    password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required,
+    repeat_password: Joi.ref('password').required,
+    email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).required
   }),
 
   employeeSignUp: Joi.object().keys({
