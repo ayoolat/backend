4feb16448e5ecd06547b34ab4170da67e0376a2f
ayoola_toluwanimi@yahoo.com <ayoola_toluwanimi@yahoo.com>
2020-12-31 15:10:38 +0100

    .

--- modules/schema.js
+++ modules/schema.js
@@ -5,8 +5,7 @@ const schemas = {
     companyName: Joi.string().alphanum().min(3).max(15).required, 
     companyType: Joi.string().required(),
     password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required,
-    repeat_password: Joi.ref('password').required,
-    email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).required
+    email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).required()
   }),
 
   employeeSignUp: Joi.object().keys({
@@ -19,6 +18,5 @@ const schemas = {
 
   changePassword: Joi.object().keys({
     password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
-    repeat_password: Joi.ref('password').required()
   })
 };
