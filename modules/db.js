8caf7edd40eeacd9a922ccb1450e90629cc217d9
ayoola_toluwanimi@yahoo.com <ayoola_toluwanimi@yahoo.com>
2021-01-02 01:14:32 +0100

    .

--- modules/db.js
+++ modules/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql')
 require('dotenv').config()
 
 //create mysql connection
-var connection = mysql.createConnection({
+var connection = mysql.createPool({
     // host: 'localhost',
     // user: 'toluwanimi',
     // password: 'password',
@@ -16,9 +16,9 @@ var connection = mysql.createConnection({
 
 // [mysql://b726afb036f0bd:3fdf3077@us-cdbr-east-02.cleardb.com/heroku_ec0bd4ee53407d5?reconnect=true]
 
-connection.connect((err, res) => {
-    if (err) throw err
-    console.log('DB connected')
-})
+// connection.connect((err, res) => {
+//     if (err) throw err
+//     console.log('DB connected')
+// })
 
 module.exports = connection
