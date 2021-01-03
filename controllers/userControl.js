14aa90e83c009260fecb0f07cf195e77cc4fa3bb
ayoola_toluwanimi@yahoo.com <ayoola_toluwanimi@yahoo.com>
2021-01-03 22:23:53 +0100

    .

--- controllers/userControl.js
+++ controllers/userControl.js
@@ -336,7 +336,7 @@ exports.addDepartment = (req, res, next) => {
     permitDetails = req.respData.response.find(x => x.permitItem == 'Edit user billing and time')
     if(permitDetails.permit === 'allowed'){
         if(permitDetails.companyID == id){
-            connection.query(`INSERT INTO company (departmentName, companyID) VALUES(${departmentName}, ${id})`,
+            connection.query(`INSERT INTO company (departmentName, companyID) VALUES('${departmentName}', '${id}')`,
             (err, resp) => {
                 // if(err) {return res.status(500).json({message: 'There has been an error, please try again'})}
                 if(err) res.send(err)
@@ -353,6 +353,34 @@ exports.addDepartment = (req, res, next) => {
         }
     }  
 }
+
+// get department
+exports.getDepartment = (res, req, next) => {
+    const {departmentName} = req.body
+    const {id} = req.params
+
+    permitDetails = req.respData.response.find(x => x.permitItem == 'Edit user billing and time')
+    if(permitDetails.permit === 'allowed'){
+        if(permitDetails.companyID == id){
+            connection.query(`SELECT * FROM department WHERE companyID = '${id}')`, (err, resp) => {
+                // if(err) {return res.status(500).json({message: 'There has been an error, please try again'})}
+                if(err) res.send(err)
+
+                if(resp){
+                    return res.json({
+                        status : 'success',
+                        data : resp
+                    })
+                } 
+            })
+        }else{
+            return res.status(500).json({message: 'You do not have permission to edit company details'})
+        }
+    }else{
+        return res.status(500).json({message: 'You do not have permission to edit company details'})
+    } 
+}
+
 
 // Update user record
 exports.updateUserRecord = (req, res, next) => {
