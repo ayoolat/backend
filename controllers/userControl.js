336d1ac1e0606be4689a7abc48a6616f209e0ca1
ayoola_toluwanimi@yahoo.com <ayoola_toluwanimi@yahoo.com>
2021-01-03 01:24:50 +0100

    .

--- controllers/userControl.js
+++ controllers/userControl.js
@@ -13,9 +13,8 @@ const authenticateToken = require('../middleware/authentication')
 const sendMail = require('../middleware/mailer')
 
 const notificationControl = require('./notificationControl')
-const { rejects } = require('assert')
 // sign up Company(post company)
-exports.signUp =  (req, res, next) =>{
+exports.signUp =  async(req, res, next) =>{
     const {companyName, email, companyType, password} = req.body
     // hash password
     bcrypt.hash(password, 10, (err, hash) => {
@@ -27,54 +26,54 @@ exports.signUp =  (req, res, next) =>{
         // handle success
         if (hash){
             connection.query = util.promisify(connection.query);
-            createCompanyAndAdmin()
-            function createCompanyAndAdmin() {
-                connection.query(`INSERT INTO company (companyName, email, companyType)
-                VALUES ('${companyName}', '${email}', '${companyType}')`, 
-                (err, resp) => {
-                    // handle error
-                    // if(err) {return res.status(500).json({message: 'There has been an error, please try again'})}
-                    if(err){
-                        return res.send(err)
-                    }
-                    // handle success
-                    // Create admin user, add details to staff table
-                    console.log('hi')
+            
+            await connection.query(`INSERT INTO company (companyName, email, companyType)
+            VALUES ('${companyName}', '${email}', '${companyType}')`, 
+            (err, resp) => {
+                // handle error
+                // if(err) {return res.status(500).json({message: 'There has been an error, please try again'})}
+                if(err){
+                    return res.send(err)
+                }
+                // handle success
+                // Create admin user, add details to staff table
+                console.log('hi')
 
-                    if(resp){
-                        
-                        connection.query(`INSERT INTO staff (companyID, password, email, roleID, username)
-                        VALUES (@@IDENTITY, '${hash}', '${email}', '1', '${email}')
-                        `, (err, resp) => {
-                            // if(err) {return res.status(500).json({message: 'There has been an error, please try again'})}
-                            if(err){
-                                return res.send(err)
-                            }
-                            console.log('hii')
+                if(resp){
+                
+                }
+            })
 
-                            if(resp){
-                                connection.query(`INSERT INTO permissions (permitID, staffID, permitItemID) VALUES ('1', @@IDENTITY, '1'), 
-                                ('1', @@IDENTITY, '2'), ('1', @@IDENTITY, '5'), ('1', @@IDENTITY, '6'), 
-                                ('1', @@IDENTITY, '7'), ('1', @@IDENTITY, '8'), ('1', @@IDENTITY, '9'), 
-                                ('1', @@IDENTITY, '10'), ('1', @@IDENTITY, '11'), ('1', @@IDENTITY, '12'), 
-                                ('1', @@IDENTITY, '13'), ('1', @@IDENTITY, '14')`, (err, resp) => {
-                                console.log('hiii')
-                                    // if(err) {return res.status(500).json({message: 'There has been an error, please try again'})}
-                                    if(err){
-                                        return res.send(err)
-                                    }
-                                    if(resp){
-                                        return res.json({
-                                            status : 'success',
-                                            data : req.body
-                                        })
-                                    }
-                                })
-                            }
-                        }) 
-                    }
-                })
-            }  
+            connection.query(`INSERT INTO staff (companyID, password, email, roleID, username)
+            VALUES (@@IDENTITY, '${hash}', '${email}', '1', '${email}')
+            `, (err, resp) => {
+                // if(err) {return res.status(500).json({message: 'There has been an error, please try again'})}
+                if(err){
+                    return res.send(err)
+                }
+                console.log('hii')
+
+                if(resp){
+                    connection.query(`INSERT INTO permissions (permitID, staffID, permitItemID) VALUES ('1', @@IDENTITY, '1'), 
+                    ('1', @@IDENTITY, '2'), ('1', @@IDENTITY, '5'), ('1', @@IDENTITY, '6'), 
+                    ('1', @@IDENTITY, '7'), ('1', @@IDENTITY, '8'), ('1', @@IDENTITY, '9'), 
+                    ('1', @@IDENTITY, '10'), ('1', @@IDENTITY, '11'), ('1', @@IDENTITY, '12'), 
+                    ('1', @@IDENTITY, '13'), ('1', @@IDENTITY, '14')`, (err, resp) => {
+                    console.log('hiii')
+                        // if(err) {return res.status(500).json({message: 'There has been an error, please try again'})}
+                        if(err){
+                            return res.send(err)
+                        }
+                        if(resp){
+                            return res.json({
+                                status : 'success',
+                                data : req.body
+                            })
+                        }
+                    })
+                }
+            }) 
+
         }
     })
 }
