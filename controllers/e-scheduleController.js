15dcd5e24cfd11be83e78adb91b5aeb5fef72691
ayoola_toluwanimi@yahoo.com <ayoola_toluwanimi@yahoo.com>
2021-01-04 20:30:25 +0100

    .

--- controllers/e-scheduleController.js
+++ controllers/e-scheduleController.js
@@ -2,7 +2,6 @@ let connection = require('../modules/db')
 
 // Creat new e-schedule
 exports.newE_schedule = (req, res, next) => {
-    console.log(req.respData.response)
     connection.query(`INSERT INTO calendar (eventName, eventDateAndTime, staffID) VALUES ('${req.body.eventName}', '${req.body.eventDateAndTime}', '${req.respData.response[0].staffID}')`, (err, resp) => {
         // if(err) {return res.status(500).json({message: 'There has been an error, please try again'})}
         if(err)res.send(err)
@@ -16,7 +15,8 @@ exports.newE_schedule = (req, res, next) => {
 }
 
 exports.getE_schedule = (req, res, next) => {
-    connection.query(`select * from calendar`, (err, resp) => {
+    const {id} = req.params
+    connection.query(`select * from calendar WHERE staffID = ${id}`, (err, resp) => {
         if(err) {return res.status(500).json({message: 'There has been an error, please try again'})}
 
         if(resp){
@@ -29,8 +29,9 @@ exports.getE_schedule = (req, res, next) => {
 }
 
 exports.editE_schedule = (req, res, next) => {
+    const {id, eventID} = req.params
     connection.query(`UPDATE calendar SET eventName = '${eventName}', 
-    eventDateAndTime = '${eventDateAndTime}', lastUpdated = NEW() WHERE staffID = ${req.params.id} AND eventID = ${req.params.eventID}`, (err, resp) => {
+    eventDateAndTime = '${eventDateAndTime}', lastUpdated = NEW() WHERE staffID = ${id} AND eventID = ${eventID}`, (err, resp) => {
         if(err) {return res.status(500).json({message: 'There has been an error, please try again'})}
 
         if(resp){
@@ -43,7 +44,8 @@ exports.editE_schedule = (req, res, next) => {
 }
 
 exports.delete = (req, res, next) => {
-    connection.query(`DELETE from calendar where eventID = ${req.params.id}`, (err, resp) => {
+    const {id} = req.params
+    connection.query(`DELETE from calendar where eventID = ${id}`, (err, resp) => {
         if(err) {return res.status(500).json({message: 'There has been an error, please try again'})}
 
         if(resp){
