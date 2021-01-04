9f909eeae8247a55bcaab52934bb8ec8d2e28802
ayoola_toluwanimi@yahoo.com <ayoola_toluwanimi@yahoo.com>
2021-01-04 21:19:53 +0100

    .

--- controllers/e-scheduleController.js
+++ controllers/e-scheduleController.js
@@ -2,7 +2,7 @@ let connection = require('../modules/db')
 
 // Creat new e-schedule
 exports.newE_schedule = (req, res, next) => {
-    connection.query(`INSERT INTO calendar (eventName, eventDateAndTime, staffID) VALUES ('${req.body.eventName}', '${req.body.eventDateAndTime}', '${req.respData.response[0].staffID}')`, (err, resp) => {
+    connection.query(`INSERT INTO e_schedule (eventName, eventDateAndTime, staffID) VALUES ('${req.body.eventName}', '${req.body.eventDateAndTime}', '${req.respData.response[0].staffID}')`, (err, resp) => {
         // if(err) {return res.status(500).json({message: 'There has been an error, please try again'})}
         if(err)res.send(err)
         if(resp){
@@ -16,7 +16,7 @@ exports.newE_schedule = (req, res, next) => {
 
 exports.getE_schedule = (req, res, next) => {
     const {id} = req.params
-    connection.query(`select * from calendar WHERE staffID = ${id}`, (err, resp) => {
+    connection.query(`select * from e_schedule WHERE staffID = ${id}`, (err, resp) => {
         if(err) {return res.status(500).json({message: 'There has been an error, please try again'})}
 
         if(resp){
@@ -30,7 +30,7 @@ exports.getE_schedule = (req, res, next) => {
 
 exports.editE_schedule = (req, res, next) => {
     const {id, eventID} = req.params
-    connection.query(`UPDATE calendar SET eventName = '${eventName}', 
+    connection.query(`UPDATE e_schedule SET eventName = '${eventName}', 
     eventDateAndTime = '${eventDateAndTime}', lastUpdated = NEW() WHERE staffID = ${id} AND eventID = ${eventID}`, (err, resp) => {
         if(err) {return res.status(500).json({message: 'There has been an error, please try again'})}
 
@@ -45,7 +45,7 @@ exports.editE_schedule = (req, res, next) => {
 
 exports.delete = (req, res, next) => {
     const {id} = req.params
-    connection.query(`DELETE from calendar where eventID = ${id}`, (err, resp) => {
+    connection.query(`DELETE from e_schedule where eventID = ${id}`, (err, resp) => {
         if(err) {return res.status(500).json({message: 'There has been an error, please try again'})}
 
         if(resp){
