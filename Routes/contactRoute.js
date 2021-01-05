16c12bb627019300371587fbdaf89fc1e55cce82
AkanWealth <akan.asanga@gmail.com>
2021-01-05 12:28:06 +0100

    removed feedback page

--- Routes/contactRoute.js
+++ Routes/contactRoute.js
@@ -1,12 +1,8 @@
 const express = require('express')
     // Export controller
-const feedbackController = require('../controllers/feedback')
+const feedbackController = require('../controllers/contact')
 
 const router = express.Router();
-
-
-// SENDING FEEDBACK
-router.post('/feedback', feedbackController.postFeedback)
 
 // CONTACT-US 
 router.post('/contact', feedbackController.postContact)
