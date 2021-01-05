9f85ced30905a6e895eff645a1b7d29f4cdaa1e6
ayoola_toluwanimi@yahoo.com <ayoola_toluwanimi@yahoo.com>
2021-01-05 20:39:55 +0100

    .,

--- Routes/usersRoute.js
+++ Routes/usersRoute.js
@@ -20,41 +20,41 @@ router.post('/signUp', validator(schema.signUp), userController.signUp);
 router.post('/login', userController.userLogin);
 
 // sign up employees
-router.post('/signUp/:companyName/addUser', authenticateToken, validator(schema.employeeSignUp), userController.employeeSignUp);
+router.post('/signUp/companyName/addUser', authenticateToken, validator(schema.employeeSignUp), userController.employeeSignUp);
 
 // // Employees second stage signup
-router.put('/:companyName/confirmation/:token/:id', authenticateToken, validator(schema.changePassword), userController.confirmSignUp);
+router.put('/companyName/confirmation/:token/:id', authenticateToken, validator(schema.changePassword), userController.confirmSignUp);
 
 // Get all users
-router.get('/:companyName/employee/:companyID',authenticateToken, userController.getAllCompanyStaff)
+router.get('/companyName/employee/:companyID',authenticateToken, userController.getAllCompanyStaff)
 
 // Update company details
-router.put('/:companyName/companyProfile/updateProfile/:id', authenticateToken, userController.updateCompanyRecord)
+router.put('/companyName/companyProfile/updateProfile/:id', authenticateToken, userController.updateCompanyRecord)
 
 // Add department
-router.put('/:companyName/companyProfile/addDepartment/:id', authenticateToken, userController.addDepartment)
+router.put('/companyName/companyProfile/addDepartment/:id', authenticateToken, userController.addDepartment)
 
 // view department
-router.get('/:companyName/companyProfile/department/:id', authenticateToken, userController.getDepartment)
+router.get('/companyName/companyProfile/department/:id', authenticateToken, userController.getDepartment)
 
 // Update user details
-router.put('/:companyName/userProfile/updateProfile/:id', authenticateToken, fileUpload.uploadImage.single('image'), userController.updateUserRecord)
+router.put('/companyName/userProfile/updateProfile/:id', authenticateToken, fileUpload.uploadImage.single('image'), userController.updateUserRecord)
 
 // view company profile
-router.get('/:companyName/companyProfile/:id',authenticateToken, userController.viewCompanyProfile)
+router.get('/companyName/companyProfile/:id',authenticateToken, userController.viewCompanyProfile)
 
 // view personal profile
-router.get('/:companyName/userProfile/:id',authenticateToken, userController.viewProfile)
+router.get('/companyName/userProfile/:id',authenticateToken, userController.viewProfile)
 
 // change password
-router.put('/:companyName/userProfile/changePassword/:id', authenticateToken, validator(schema.changePassword), userController.changePassword)
+router.put('/companyName/userProfile/changePassword/:id', authenticateToken, validator(schema.changePassword), userController.changePassword)
 
 // Manage employee time and billing details
-router.put('/:companyName/employee/timeAndBilling/:id', authenticateToken, userController.timeAndBilling)
+router.put('/companyName/employee/timeAndBilling/:id', authenticateToken, userController.timeAndBilling)
 
 // reset password
-router.post('/:companyName/userProfile/forgot-password', userController.resetPassword)
-router.put('/:companyName/userProfile/passwordReset/:token/:id', authenticateToken, validator(schema.changePassword),userController.setNewPassword)
+router.post('/companyName/userProfile/forgot-password', userController.resetPassword)
+router.put('/companyName/userProfile/passwordReset/:token/:id', authenticateToken, validator(schema.changePassword),userController.setNewPassword)
 
 
 
