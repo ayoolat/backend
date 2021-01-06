9c086d967931d0b1c4dab221a39f9097500c50c9
ayoola_toluwanimi@yahoo.com <ayoola_toluwanimi@yahoo.com>
2021-01-06 04:00:00 +0100

    .,

--- controllers/taskSheetController.js
+++ controllers/taskSheetController.js
@@ -33,7 +33,9 @@ exports.taskSheetDepartment = (req, res, next) => {
     permitDetails = req.respData.response.find(x => x.permitItem == 'View department timesheet and billing report')
     if(permitDetails.permit === 'allowed'){
         connection.query(`SELECT t.taskName, S.firstName, S.lastName, T.startDate, T.endDate, ST.taskStatus, t.lastUpdated FROM task T 
-        JOIN staff S ON T.staffID = S.staffID JOIN status ST ON T.taskStatus = ST.statusID WHERE companyID = ${id} AND departmentID = ${departmentID}`, (err, resp) => {
+        LEFT JOIN staff S ON T.staffID = S.staffID 
+        JOIN status ST ON T.taskStatus = ST.statusID 
+        WHERE companyID = ${id} AND departmentID = ${departmentID}`, (err, resp) => {
 
             // if(err) {return res.status(500).json({message: 'There has been an error, please try again'})}
             if(err) res.send(err)
