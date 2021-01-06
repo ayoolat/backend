3fd0c6deef6516ac5f1d37cd22f29c6ad1deb2b6
ayoola_toluwanimi@yahoo.com <ayoola_toluwanimi@yahoo.com>
2021-01-06 03:55:04 +0100

    .,

--- controllers/taskSheetController.js
+++ controllers/taskSheetController.js
@@ -35,8 +35,8 @@ exports.taskSheetDepartment = (req, res, next) => {
         connection.query(`SELECT t.taskName, S.firstName, S.lastName, T.startDate, T.endDate, ST.taskStatus, t.lastUpdated FROM task T 
         JOIN staff S ON T.staffID = S.staffID JOIN status ST ON T.taskStatus = ST.statusID WHERE companyID = ${id} AND departmentID = ${dept}`, (err, resp) => {
 
-            if(err) {return res.status(500).json({message: 'There has been an error, please try again'})}
-
+            // if(err) {return res.status(500).json({message: 'There has been an error, please try again'})}
+            if(err) res.send(err)
             if(resp){
                 return res.json({
                     status : 'success',
