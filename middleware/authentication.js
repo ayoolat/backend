a25fad2df60a13bafcd87482e5f29eb922b22005
ayoola_toluwanimi@yahoo.com <ayoola_toluwanimi@yahoo.com>
2021-01-08 23:30:25 +0100

    .

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -5,29 +5,16 @@ authenticateToken = function (req, res, next){
     const authenticationHeader = req.headers['authorization']
     const Token = authenticationHeader && authenticationHeader.split(' ')[1]
 
-    if(Token == null) return res.status(401).send('invalid request')
+    if(Token == null) return res.status(401).json({error:'Unauthorized request'})
 
     jwt.verify(Token, process.env.ACCESS_TOKEN_KEY, (err, data) => {
-        if(err) return res.status(401).json({
-            error:'invalid token or token expired'
-        })
+        // if(err) return res.status(401).json({error:'invalid token or token expired'})
+        if(err) return res.status(401).send(err)
         req.respData = data
         next()
     })
 }
 
 module.exports = authenticateToken
-// exports.authenticateTokens = function (reqAuth, resAuth, next){
-//     const jwt = require('jsonwebtoken')
-//     const authenticationHeader = reqAuth.headers['authorization']
-//     const Token = authenticationHeader && authenticationHeader.split(' ')[1]
 
-//     if(Token == null) return resAuth.status(401).send('Token expired')
-
-//     jwt.verify(Token, process.env.ACCESS_TOKEN_KEY, (err, data) => {
-//         if(err) return resAuth.send('invalid token or token expired')
-//         reqAuth.respData = data
-//         next()
-//     })
-//     }
 
