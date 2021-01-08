1534d5474b04a8a4ddd7449f07917b145369cc06
ayoola_toluwanimi@yahoo.com <ayoola_toluwanimi@yahoo.com>
2021-01-09 00:05:58 +0100

    .

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -5,7 +5,9 @@ authenticateToken = function (req, res, next){
     const authenticationHeader = req.headers['authorization']
     const Token = authenticationHeader && authenticationHeader.split(' ')[1]
 
+    // if(Token == null) return res.status(401).json({error:'Unauthorized request'})
     if(Token == null) return res.status(401).json({error:'Unauthorized request'})
+
 
     jwt.verify(Token, process.env.ACCESS_TOKEN_KEY, (err, data) => {
         // if(err) return res.status(401).json({error:'invalid token or token expired'})
