0c2bae7a792fe1c206263d3c9e2eb5dd7300e1b2
ayoola_toluwanimi@yahoo.com <ayoola_toluwanimi@yahoo.com>
2021-01-08 23:14:57 +0100

    .

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -8,7 +8,9 @@ authenticateToken = function (req, res, next){
     if(Token == null) return res.status(401).send('invalid request')
 
     jwt.verify(Token, process.env.ACCESS_TOKEN_KEY, (err, data) => {
-        if(err) return res.send('invalid token or token expired')
+        if(err) return res.status(401).json({
+            error:'invalid token or token expired'
+        })
         req.respData = data
         next()
     })
