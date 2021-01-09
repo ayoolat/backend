f50fe7753d9245b331440e88dcce5867cd220b92
ayoola_toluwanimi@yahoo.com <ayoola_toluwanimi@yahoo.com>
2021-01-09 02:42:39 +0100

    .

--- controllers/managePermissionsController.js
+++ controllers/managePermissionsController.js
@@ -15,7 +15,7 @@ exports.addPermission = (req, res, next) => {
             if(err)res.send(err)
 
             if(resp){
-                connection.query(`SELECT PI.permitItem, PI.permitID FROM permitItem PI JOIN permissions PE ON PI.permitItemID = PE.permitItemId`, (err, respQuery) => {
+                connection.query(`SELECT PI.permitItem, PE.permitID FROM permitItem PI JOIN permissions PE ON PI.permitItemID = PE.permitItemId`, (err, respQuery) => {
                     if(err){
                         return res.send(err)
                     }
