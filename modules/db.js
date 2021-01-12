4e48f619fb8dc9bca2ec49db41cb7b08159c858f
ayoola_toluwanimi@yahoo.com <ayoola_toluwanimi@yahoo.com>
2021-01-12 16:19:58 +0100

    .

--- modules/db.js
+++ modules/db.js
@@ -4,17 +4,17 @@ require('dotenv').config()
 
 // create mysql connection
 const connection = mysql.createPool({
-    host: 'localhost',
-    user: 'toluwanimi',
-    password: 'password',
-    database: 'pacetimesheet_'
-    // host: process.env.db_host,
-    // user: process.env.db_user,
-    // password: process.env.db_password,
-    // database: process.env.db_database,
-    // waitForConnections: true,
-    // connectionLimit: 10,
-    // queueLimit: 0
+    // host: 'localhost',
+    // user: 'toluwanimi',
+    // password: 'password',
+    // database: 'pacetimesheet_'
+    host: process.env.db_host,
+    user: process.env.db_user,
+    password: process.env.db_password,
+    database: process.env.db_database,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 })
 
 
