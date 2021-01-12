538d7c8c086a5b43737e1ae00b5d19441b2df4d5
ayoola_toluwanimi@yahoo.com <ayoola_toluwanimi@yahoo.com>
2021-01-12 15:31:16 +0100

    .

--- modules/db.js
+++ modules/db.js
@@ -2,38 +2,37 @@
 const mysql = require('mysql')
 require('dotenv').config()
 
-// create mysql connection
-// var connection = mysql.createPool({
-//     // host: 'localhost',
-//     // user: 'toluwanimi',
-//     // password: 'password',
-//     // database: 'pacetimesheet_'
-//     host: process.env.db_host,
-//     user: process.env.db_user,
-//     password: process.env.db_password,
-//     database: process.env.db_database,
-//     waitForConnections: true,
-//     connectionLimit: 10,
-//     queueLimit: 0
-// })
-
-
-// connection.connect((err, res) => {
-//     if (err) throw err
-//     console.log('DB connected')
-// })
-
-const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "pacetimesheet_"
+var connection = mysql.createPool({
+    // host: 'localhost',
+    // user: 'toluwanimi',
+    // password: 'password',
+    // database: 'pacetimesheet_'
+    host: process.env.db_host,
+    user: process.env.db_user,
+    password: process.env.db_password,
+    database: process.env.db_database,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 })
+
 
 connection.connect((err, res) => {
     if (err) throw err
-
-    console.log("db server running")
+    console.log('DB connected')
 })
+
+// const connection = mysql.createConnection({
+//     host: "localhost",
+//     user: "root",
+//     password: "",
+//     database: "pacetimesheet_"
+// })
+
+// connection.connect((err, res) => {
+//     if (err) throw err
+
+//     console.log("db server running")
+// })
 
 module.exports = connection
