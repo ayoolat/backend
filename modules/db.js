c1aec3733be212609fd2f609fcdbf1c94eadb543
ayoola_toluwanimi@yahoo.com <ayoola_toluwanimi@yahoo.com>
2021-01-12 16:05:14 +0100

    .

--- modules/db.js
+++ modules/db.js
@@ -2,17 +2,38 @@
 const mysql = require('mysql')
 require('dotenv').config()
 
-//create mysql connection
-var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'toluwanimi',
-    password: 'password',
-    database: 'pacetimesheet_'
-    
+// create mysql connection
+const connection = mysql.createPool({
+    // host: 'localhost',
+    // user: 'toluwanimi',
+    // password: 'password',
+    // database: 'pacetimesheet_'
+    host: process.env.db_host,
+    user: process.env.db_user,
+    password: process.env.db_password,
+    database: process.env.db_database,
+    waitForConnections: true,
+    connectionLimit: 20,
+    queueLimit: 0
 })
 
-// [mysql://b726afb036f0bd:3fdf3077@us-cdbr-east-02.cleardb.com/heroku_ec0bd4ee53407d5?reconnect=true]
 
+connection.connect((err, res) => {
+    if (err) throw err
+    console.log('DB connected')
+})
 
+// const connection = mysql.createConnection({
+//     host: "localhost",
+//     user: "root",
+//     password: "",
+//     database: "pacetimesheet_"
+// })
+
+// connection.connect((err, res) => {
+//     if (err) throw err
+
+//     console.log("db server running")
+// })
 
 module.exports = connection
