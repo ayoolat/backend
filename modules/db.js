61d833fe72550cfa9641ef8ac5a9427c92bd4b5f
ayoola_toluwanimi@yahoo.com <ayoola_toluwanimi@yahoo.com>
2021-01-12 16:42:33 +0100

    .

--- modules/db.js
+++ modules/db.js
@@ -3,11 +3,7 @@ const mysql = require('mysql')
 require('dotenv').config()
 
 // create mysql connection
-<<<<<<< HEAD
 const connection = mysql.createPool({
-=======
-var connection = mysql.createPool({
->>>>>>> 5b290b72ef669b3fe3b062da532e03f78649d0db
     // host: 'localhost',
     // user: 'toluwanimi',
     // password: 'password',
@@ -20,7 +16,6 @@ var connection = mysql.createPool({
     connectionLimit: 10,
     queueLimit: 0
 })
-<<<<<<< HEAD
 
 
 // connection.connect((err, res) => {
@@ -40,14 +35,6 @@ var connection = mysql.createPool({
 
 //     console.log("db server running")
 // })
-=======
-
-
-connection.connect((err, res) => {
-    if (err) throw err
-    console.log('DB connected')
-})
->>>>>>> 5b290b72ef669b3fe3b062da532e03f78649d0db
 
 // const connection = mysql.createConnection({
 //     host: "localhost",
